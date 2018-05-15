71873b30262479d180f2fb5ef2c84c622de9029e
ChrisChrisLoLo <sporewoh@gmail.com>
2018-05-15 14:26:51 -0600

    restructured file directory

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const port = 3000;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var drawRouter = require('./routes/draw');
+
 var app = express();
 
-app.use(express.static(__dirname+"/public"));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.get('/',(req,res) =>{
-	res.sendFile(path.join(__dirname+'/views/index.html'));
-});
-app.get('/draw',(req,res) =>{
-	res.sendFile(path.join(__dirname+'/views/draw/draw.html'));
-});
-app.get('/login',(req,res)=>{
-	res.sendFile(path.join(__dirname+'/views/login.html'));
-});
-// // /bad - send back json with errorMessage
-// app.get('/bad', (req, res) => {
-// 	res.send({
-// 		errorMessage: 'Unable to handle request'
-// 	});
-// });
-app.get('*',(req,res)=>{
-	res.status(404).send("Page not found :(");
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/draw', drawRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, () => {
-	console.log(`Server is up on port ${port}`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
