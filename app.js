5e4263860904f1fc50d7d778047f87ebcf808ef4
ChrisChrisLoLo <sporewoh@gmail.com>
2018-05-19 21:24:43 -0600

    Templating now works as it should

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,11 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials/layouts');
+hbs.registerPartials(__dirname + '/views/partials/includes');
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
