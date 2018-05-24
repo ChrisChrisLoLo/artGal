fd5348b4f6e80d00c046c0c33bff706adb3c329f
ChrisChrisLoLo <sporewoh@gmail.com>
2018-05-23 21:03:28 -0600

    Commiting since im going to try to reformat bootstrap on draw page

--- public/javascripts/drawpad.js
+++ public/javascripts/drawpad.js
@@ -4,6 +4,7 @@
 var canvas = document.getElementById('canvasHTML');
 var ctx = canvas.getContext('2d');
 var mousePressed = false;
+var oldImageData;
 var prevPos = {x:0,y:0};
 var pos = {x:0,y:0};
 var brush = {
@@ -11,21 +12,28 @@ var brush = {
 	colour:"#000000",
 }
 
-canvas.addEventListener("mousedown", function (e) {
+canvas.addEventListener("mousedown", function(e){
 	pos = getMousePos(e);
 	prevPos = getMousePos(e);
 	mousePressed=true;
 	draw(e);
 }, false);
-canvas.addEventListener("mousemove", function (e) {
+canvas.addEventListener("mousemove", function(e){
+	// undrawCursor(e);
+	// drawCursor(e);
 	draw(e);
 }, false);
-canvas.addEventListener("mouseup", function (e) {
+canvas.addEventListener("mouseup", function(e){
 	mousePressed=false
 }, false);
-// canvas.addEventListener("mouseout", function (e) {
-// 	mousePressed=false
-// }, false);
+//TODO, have mousePressed = false if mouse is out and mouse is not down
+canvas.addEventListener("mouseout", function(e){
+	mousePressed=false
+},false);
+canvas.addEventListener("mouseover", function(e){
+	prevPos = pos;
+	pos = getMousePos(e);
+},false)
 ctx.fillStyle = 'white';
 	ctx.fillRect(0,0,canvas.width, canvas.height);
 
@@ -40,11 +48,11 @@ function getMousePos(e) {
 
 function draw(e){
 	"use strict";
+	prevPos = pos;
+	pos = getMousePos(e);
 	if (mousePressed){
 		//Draws a line for the last mouse position to the current mouse postion
 		//and fills in a circle at the new position. The circle is to smooth out lines
-		prevPos = pos;
-		pos = getMousePos(e);
 		ctx.lineWidth = brush.size;
 		ctx.beginPath();
 		ctx.moveTo(prevPos.x,prevPos.y);
@@ -60,6 +68,17 @@ function draw(e){
 	}
 }
 
+// function drawCursor(e){
+// 	oldImageData = ctx.getImageData(pos.x-brush.size,pos.y-brush.size,brush.size*2,brush.size*2);
+// 	ctx.lineWidth = 1;
+// 	ctx.beginPath();
+// 	ctx.arc(pos.x,pos.y,brush.size/2,0,2*Math.PI);
+// 	ctx.stroke();
+// }
+
+// function undrawCursor(e){
+// 	ctx.
+// }
 //NOTE: Currently there is redundency in terms of assigning the value to both the brush property
 // as will as the ctx attributes. Currently there is no need for the brush variables whatsoever
 // as the ctx attributes can just be changed directly, this may change however, therefore both methods
