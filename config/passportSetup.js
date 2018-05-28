5a138ad71db549b0aefd0bc2288faa01ffe9c797
ChrisChrisLoLo <sporewoh@gmail.com>
2018-05-28 15:39:57 -0600

    Users can now be added through google sign in without duplication.

--- config/passportSetup.js
+++ config/passportSetup.js
@@ -14,15 +14,23 @@ passport.use(
         //Passport callback function
         //We want the profile information for logging in
         console.log(profile);
-        //TODO:import user image or have a default image.
-        new User({
-            googleID: profile.id,
-            displayName: profile.displayName,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            gender: profile.gender
-        }).save().then((newUser) => {
-            console.log(`New user: ${newUser}`)
+        User.findOne({googleID:profile.id}).then((currentUser)=>{
+            if(currentUser){
+                //already have the user
+                console.log('User currently exists');
+            }else{
+                //TODO:import user image or have a default image.
+                new User({
+                    googleID: profile.id,
+                    displayName: profile.displayName,
+                    firstName: profile.name.givenName,
+                    lastName: profile.name.familyName,
+                    gender: profile.gender
+                }).save().then((newUser) => {
+                    console.log(`New user: ${newUser}`)
+                });
+            }
         });
+        
     })
 );
