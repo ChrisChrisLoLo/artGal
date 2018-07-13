bd1776e99350de904141db4c036128c86a5dc323
ChrisChrisLoLo <sporewoh@gmail.com>
2018-07-12 20:57:45 -0600

    Added dummy pages as well as tweaked button positioning

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,25 @@ router.get('/', drawings_controller.drawing_list);
 
 router.get('/p/:pageCount', drawings_controller.drawing_list);
 
+router.get('/privacy', (req,res)=>{
+    res.render('privacy',{
+        title:'Privacy',
+        user:req.user
+    });
+});
+
+router.get('/terms', (req,res)=>{
+    res.render('privacy',{
+        title:'Terms',
+        user:req.user
+    });
+});
+
+router.get('/support', (req,res)=>{
+    res.render('privacy',{
+        title:'Support',
+        user:req.user
+    });
+});
+
 module.exports = router;
